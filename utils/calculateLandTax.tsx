e98f0facaf641e5102e6a83d089e2b5565761b8e
anikal2001 <anikal2001@gmail.com>
2020-12-19 14:46:46 -0500

    Fixed less than 200 bug

--- utils/calculateLandTax.tsx
+++ utils/calculateLandTax.tsx
@@ -11,7 +11,12 @@ export default function calculateLandTax(HomePrice: string){
         tax += homePrice* (0.5/100)
     }
     if (homePrice > 55000){
-        tax += ((195000)*(1/100))
+        if (homePrice < 250000){
+            tax += (homePrice-55000)*(1/100)
+        }
+        else{
+            tax += ((195000)*(1/100))
+        }
     }
     if(homePrice >= 250000){
         if ( tax+ ((homePrice-250000)*(1.5/100)) < 4000){
